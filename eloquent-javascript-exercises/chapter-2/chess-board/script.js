9a3ff14fd9a2c5380e01115ae0a619fb992c4e36
Chris Pearce <crite70@hotmail.com>
2015-10-31 12:24:34 +1100

    finishing chessboard excercise

--- eloquent-javascript-exercises/chapter-2/chess-board/script.js
+++ eloquent-javascript-exercises/chapter-2/chess-board/script.js
@@ -1,17 +1,79 @@
+/**
+ * Write a program that creates a string that represents an 8×8 grid, using
+ * newline characters to separate lines. At each position of the grid there is
+ * either a space or a “#” character. The characters should form a chess board.
+ *
+ * Passing this string to console.log should show something like this:
+ *
+     # # # #
+    # # # #
+     # # # #
+    # # # #
+     # # # #
+    # # # #
+     # # # #
+    # # # #
+ *
+ * When you have a program that generates this pattern, define a variable size
+ * = 8 and change the program so that it works for any size, outputting a grid
+ * of the given width and height.
+ *
+ * http://eloquentjavascript.net/02_program_structure.html#h_5Hz2kiaaXp
+ */
+
+
 var chessBoard = function() {
 
-  var rowO = " # # # #";
-  var rowE = "# # # #";
+  var hash          = "#";
+  var space         = " ";
+  var lineBreak     = "\n";
+  var gridSize      = 8;
+  var chars         = "";
 
-  for (var i = 0; i < 7; i++) {
-
-    for (var j = 0; j < 7; j++) {
-      console.log(rowE);
+  for (var i = 0; i < gridSize; i++) {
+    if (i % 2 === 0) {
+        for (var j = 0; j < gridSize; j++) {
+            if (j % 2 === 0) {
+                chars = chars + hash;
+            } else {
+                chars = chars + space;
+            }
+        }
+    } else {
+        for (var j = 0; j < gridSize; j++) {
+            if (j % 2 === 0) {
+                chars = chars + space;
+            } else {
+                chars = chars + hash;
+            }
+        }
     }
-
-    console.log(rowO);
+    chars = chars + lineBreak;
   }
+
+  console.log(chars);
 
 };
 
-chessBoard();
+
+var chessBoardAlt = function() {
+
+  var gridSize      = 8;
+  var chars         = "";
+
+  for (var i = 0; i < gridSize; i++) {
+    for (var j = 0; j < gridSize; j++) {
+        if ((i + j) % 2 === 0) {
+            chars = chars + "#";
+        } else {
+            chars = chars + " ";
+        }
+    }
+    chars = chars + "\n";
+  }
+
+  console.log(chars);
+
+};
+
+chessBoardAlt();
--- eloquent-javascript-exercises/chapter-2/looping-a-triangle/script.js
+++ eloquent-javascript-exercises/chapter-2/looping-a-triangle/script.js
@@ -1,3 +1,22 @@
+/**
+ * Write a loop that makes seven calls to console.log to output the following
+ * triangle:
+
+    #
+    ##
+    ###
+    ####
+    #####
+    ######
+    #######
+
+ *
+ * It may be useful to know that you can find the length of a string by
+ * writing .length after it.
+ *
+ * http://eloquentjavascript.net/02_program_structure.html#h_umoXp9u0e7
+ */
+
 var loopingATriange = function() {
 
   var hash = "#";
